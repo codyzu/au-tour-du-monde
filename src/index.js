7709f044477edc1511b3eaf2e649353ee38909af
Cody Zuschlag <cody.zuschlag@gmail.com>
2019-04-09 18:38:44 +0200

    add: initial index page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootswatch/dist/cyborg/bootstrap.css'; // eslint-disable-line import/no-unassigned-import
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
